5370c2dcaba9e7913f9ecce5f47aaae726aec58a
kevine-developper <kevinarson_1996@yahoo.fr>
2025-09-17 23:28:39 +0200

    feat: Add "use client" directive to multiple components for client-side rendering

--- app/components/InstitutionDetail.tsx
+++ app/components/InstitutionDetail.tsx
@@ -1,3 +1,4 @@
+"use client"
 import React from "react";
 import { Institution } from "../types";
 import { X } from "lucide-react";
--- app/components/map/MapControlButton.tsx
+++ app/components/map/MapControlButton.tsx
@@ -1,3 +1,4 @@
+"use client";
 import { ComponentType } from "react";
 
 type MapControlButtonProps = {
